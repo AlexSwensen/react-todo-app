e7788f045f11c455dc3a0c7e8fc990442b6cbab8
Alexander Swensen <alex.swensen@gmail.com>
2021-06-02 15:05:47 -0500

    start with a real todo list container

---
+++ src/components/todo-list/todoList.tsx
@@ -0,0 +1,12 @@
+import './todoList.scss';
+
+export const TodoList = () => {
+    return (
+        <div className="todo-list-container">
+            <h2>Todo List</h2>
+            <div className="todo-list-box">
+                this is the box
+            </div>
+        </div>
+    )
+}
