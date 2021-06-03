5af46778095575a4231c043b94e5474f892aa452
Alexander Swensen <alex.swensen@gmail.com>
2021-06-03 10:28:39 -0500

    submit button is better placed now

--- src/components/todo-item-input/todoItemInput.tsx
+++ src/components/todo-item-input/todoItemInput.tsx
@@ -20,13 +20,11 @@ export const TodoItemInput = (props: ITodoListInputProps) => {
     return (
         <div className="todoItem">
             <form onSubmit={handleSubmit}>
-                <div className="input">
                 <input 
                     type="text" 
                     value={name}
                     onChange={e => setName(e.target.value)}
                 />
-                </div>
                 <input type="submit" title="Add Item" value="Add Item" disabled={!inputDisabled()}/>
             </form>
         </div>
